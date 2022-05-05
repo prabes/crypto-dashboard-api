6462d85021fcf54f21f2d2fc538fa278aa63db6a
prabes <dprabesh11@gmail.com>
2022-05-05 17:19:15 +0545

    add Models and controllers

---
+++ controllers/cryptoDetails.controllers.js
@@ -0,0 +1,61 @@
+const sequelize = require('sequelize');
+const CryptoDetail = require("../models/cryptoDetails");
+const Op = sequelize.Op;
+
+const getFindOptions = ({ pageNumber=1,
+  perPage=100,
+  sortColumn='name',
+  sortOrder='ASC',
+  searchQuery="" }) => {
+  if (!searchQuery) {
+    return {
+      where: {
+        [Op.or]: {
+          symbol: { [Op.like]: '%' + searchQuery + '%' },
+          name: { [Op.like]: '%' + searchQuery + '%' },
+        }
+      },
+      offset: pageNumber,
+      limit: perPage,
+      order: [
+        [sortColumn, sortOrder],
+      ]
+    }
+  }
+  return {
+    offset: pageNumber,
+    limit: perPage,
+    order: [
+      [sortColumn, sortOrder],
+    ]
+  }
+}
+
+const index = async (req, res) => {
+  const {
+    pageNumber,
+    perPage,
+    sortColumn,
+    sortOrder,
+    searchQuery
+  } = req.query;
+
+  try {
+    console.log("PPP", getFindOptions(req.query))
+    const { count, rows: details } = await CryptoDetail.findAndCountAll(getFindOptions(req.query));
+
+    return res.status(200).json({
+      details: details,
+      perPage: perPage,
+      pageNumber: pageNumber,
+      total: count
+    });
+  } catch (e) {
+    console.log("Error while fetching crypto details");
+    return res.status(500).json({ error: e.message });
+  }
+};
+
+module.exports = {
+  index
+};
--- models/cryptoDetails.js
+++ models/cryptoDetails.js
@@ -1,42 +1,49 @@
-const sequelize = require("../config/db")
-
-const { DataTypes } = require("sequelize")
+const sequelize = require("../config/db");
+const { DataTypes } = require("sequelize");
 
 const CryptoDetail = sequelize.define("crypto_details", {
 	id: {
 		type: DataTypes.INTEGER,
-		autoIncrement: true,
 		primaryKey: true,
+		allowNull: false,
+		autoIncrement: true,
 	},
 	coinId: {
 		type: DataTypes.INTEGER,
+    allowNull: false
 	},
 	name: {
 		type: DataTypes.STRING,
+    allowNull: false
 	},
 	symbol: {
 		type: DataTypes.STRING,
+    allowNull: false
 	},
 	slug: {
 		type: DataTypes.STRING,
+    allowNull: false
+	},
+	circulating_supply: {
+		type: DataTypes.FLOAT,
 	},
 	max_supply: {
-		type: DataTypes.STRING,
+		type: DataTypes.FLOAT,
 	},
 	price_usd: {
-		type: DataTypes.STRING,
+		type: DataTypes.FLOAT,
 	},
 	volume_24hr: {
-		type: DataTypes.INTEGER,
+		type: DataTypes.FLOAT,
 	},
 	percentage_change_24h: {
-		type: DataTypes.INTEGER,
+		type: DataTypes.FLOAT,
 	},
 	percentage_change_7d: {
-		type: DataTypes.INTEGER,
+		type: DataTypes.FLOAT
 	},
 	market_cap: {
-		type: DataTypes.INTEGER,
+		type: DataTypes.FLOAT,
 	},
 	cmc_rank: {
 		type: DataTypes.INTEGER,
@@ -44,31 +51,24 @@ const CryptoDetail = sequelize.define("crypto_details", {
 	total_supply: {
 		type: DataTypes.INTEGER,
 	},
-
-	last_updated: {
-		type: DataTypes.STRING,
+	volume_change_24hr: {
+		type: DataTypes.FLOAT,
 	},
-	date_added: {
-		type: DataTypes.STRING,
+	percentage_change_1h: {
+		type: DataTypes.FLOAT,
 	},
-
-	// volume_change_24hr: {
-	// 	type: DataTypes.INTEGER,
-	// 	allowNull: false,
-	// },
-	// percentage_change_1h: {
-	// 	type: DataTypes.INTEGER,
-	// 	allowNull: false,
-	// },
-
-	// market_cap_dominance: {
-	// 	type: DataTypes.INTEGER,
-	// 	allowNull: false,
-	// },
-	// fully_diluted_market_cap: {
-	// 	type: DataTypes.INTEGER,
-	// 	allowNull: false,
-	// },
-})
+	market_cap_dominance: {
+		type: DataTypes.FLOAT,
+	},
+	fully_diluted_market_cap: {
+		type: DataTypes.FLOAT,
+	},
+	createdAt: {
+		type: DataTypes.DATE,
+	},
+	updatedAt: {
+		type: DataTypes.DATE,
+	}
+});
 
 module.exports = CryptoDetail;
